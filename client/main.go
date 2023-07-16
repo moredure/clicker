82422fd32e3648a35f7562e205eb633078dd6d9a
Mykhailo Faraponov <mfaraponov@cloudlinux.com>
2023-07-16 12:33:54 +0300

    refactored

--- client/main.go
+++ client/main.go
@@ -4,24 +4,38 @@ import (
 	"bufio"
 	"context"
 	"github.com/gorilla/websocket"
+	"log"
 	"os"
+	"sync"
 )
 
 func main() {
 	s := bufio.NewReader(os.Stdin)
 	line, _, err := s.ReadLine()
 	var d websocket.Dialer
-	conn, _, err := d.DialContext(context.Background(), "http://localhost:80/game?username="+string(line), nil)
+	conn, _, err := d.DialContext(context.Background(), "ws://localhost:80/game?username="+string(line), nil)
 	if err != nil {
 		panic(err)
 	}
-	// states as well
-	for {
-		conn.ReadMessage()
-		line, _, err := s.ReadLine()
-		err := conn.WriteMessage(websocket.TextMessage, []byte("tap"))
-		if err != nil {
-			panic(err)
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		for {
+			_, data, err := conn.ReadMessage()
+			if err != nil {
+				log.Println("close", string(data))
+				return
+			}
+			log.Println(string(data))
 		}
-	}
+	}()
+	go func() {
+		defer wg.Done()
+		for {
+			s.ReadLine()
+			conn.WriteMessage(websocket.TextMessage, []byte("tap"))
+		}
+	}()
+	wg.Wait()
 }
